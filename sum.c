3cfe9a3f14e11821a2f1d1640a1383e851447383
prarthana10101 <prarthana10101m@gmail.com>
2026-01-31 15:54:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    int* ans = (int*)malloc(2 * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                ans[0] = i;
+                ans[1] = j;
+                return ans;
+            }
+        }
+    }
+    return NULL; // guaranteed one solution
+}
+
